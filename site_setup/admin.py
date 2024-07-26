9e2c4dead593a1207e4fcb1fefe7820a76ff0e29
marciosebastiao4758 <marciosebastiao302626@hotmail.com>
2024-07-26 16:26:32 -0300

    create class MenuLink e SiteSetup

--- site_setup/admin.py
+++ site_setup/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
+from django.http import HttpRequest
 
-from .models import Menulink
+from .models import Menulink, SiteSetup
 
 @admin.register(Menulink)
 class MenuLinkAdmin(admin.ModelAdmin):
     list_display = ["id","text", "url_or_path",]
     list_display_links = ["id","text", "url_or_path",]
     search_fields = ["id","text", "url_or_path",]
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = ["title","description",]
+ 
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.exists()
+    
     
     
